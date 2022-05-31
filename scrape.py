1548bca0b53343129d504c9fc57a594a28b92b5c
jeet7223 <jeetparmar7223@gmail.com>
2022-05-31 15:04:27 +0530

    configuration.py added and browser customization code

--- scrape.py
+++ scrape.py
@@ -5,14 +5,28 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import configuration
 
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument("window-size=1920,1080")
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument('ignore-certificate-errors')
+if configuration.headless_mode:
+    options.add_argument('--headless')
 
-# Chrome
-driver = webdriver.Chrome(ChromeDriverManager().install())
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_experimental_option("useAutomationExtension", False)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+
 driver.implicitly_wait(5)
 
 driver.get("https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&q=Stock%20Market&search_type=keyword_unordered&media_type=all")
-driver.maximize_window()
 
 p = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "_9cb_")))
 time.sleep(2)
